7132f560b285721eda3d077368dbd74a6b1cd576
varabyeu <varabyeu.v@yandex.by>
2023-04-09 17:18:03 +0300

    initial

---
+++ main.py
@@ -0,0 +1,136 @@
+import re
+import uuid
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.routing import APIRouter
+from pydantic import BaseModel, EmailStr, validator
+from sqlalchemy import Column, Boolean, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+import settings
+
+engine = create_async_engine(
+    settings.REAL_DATABASE_URL,
+    future=True,
+    echo=True
+)
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    is_active = Column(Boolean, default=True)
+
+
+class UserDAL:
+    """Data access layer for operating user info"""
+
+    def __int__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(
+            self,
+            name: str,
+            surname: str,
+            email: str,
+    ) -> User:
+        new_user = User(
+            name=name,
+            surname=surname,
+            email=email
+        )
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
+
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-яa-zA-Z\-]+$")
+
+
+class TunedModel(BaseModel):
+    class Config:
+        """Tells pydantic to convert even non-dict obj to json"""
+
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Name should contain only letters"
+            )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422,
+                detail="Surname should contain only letters"
+            )
+        return value
+
+
+class ShowUser(TunedModel):
+    user_id: int
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+app = FastAPI("Vitali")
+
+user_router = APIRouter()
+
+
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active
+            )
+
+
+@user_router.post('/', response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+main_api_router = APIRouter()
+main_api_router.include_router(user_router, prefix='/user', tags=["user"])
+app.include_router(main_api_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
